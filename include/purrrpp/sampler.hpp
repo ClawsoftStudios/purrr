b8d74d55ff97928a1fcbbadc9db1e3db44b94618
RealCatDev <real.catdev@gmail.com>
2025-05-18 01:38:53 +0200

    Renamed sampler enums

--- include/purrrpp/sampler.hpp
+++ include/purrrpp/sampler.hpp
@@ -7,24 +7,24 @@ class Sampler : public Wrapper<Purrr_Sampler> {
   friend class Context;
 public:
   enum class Filter {
-    Nearest = PURRR_NEAREST,
-    Linear = PURRR_LINEAR
+    Nearest = PURRR_SAMPLER_FILTER_NEAREST,
+    Linear  = PURRR_SAMPLER_FILTER_LINEAR
   };
 
   enum class AddressMode {
-    Repeat = PURRR_SAMPLER_ADDRESS_REPEAT,
-    Mirror = PURRR_SAMPLER_ADDRESS_MIRROR,
-    ClampToEdge = PURRR_SAMPLER_ADDRESS_CLAMP_TO_EDGE,
-    ClampToBorder = PURRR_SAMPLER_ADDRESS_CLAMP_TO_BORDER
+    Repeat        = PURRR_SAMPLER_ADDRESS_MODE_REPEAT,
+    Mirror        = PURRR_SAMPLER_ADDRESS_MODE_MIRROR,
+    ClampToEdge   = PURRR_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE,
+    ClampToBorder = PURRR_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER
   };
 
   enum class BorderColor {
-    FloatTransparentBlack = PURRR_SAMPLER_BORDER_FLOAT_TRANSPARENT_BLACK,
-    IntTransparentBlack = PURRR_SAMPLER_BORDER_INT_TRANSPARENT_BLACK,
-    FloatOpaqueBlack = PURRR_SAMPLER_BORDER_FLOAT_OPAQUE_BLACK,
-    IntOpaqueBlack = PURRR_SAMPLER_BORDER_INT_OPAQUE_BLACK,
-    FloatOpaqueWhite = PURRR_SAMPLER_BORDER_FLOAT_OPAQUE_WHITE,
-    IntOpaqueWhite = PURRR_SAMPLER_BORDER_INT_OPAQUE_WHITE
+    FloatTransparentBlack = PURRR_SAMPLER_BORDER_COLOR_FLOAT_TRANSPARENT_BLACK,
+    IntTransparentBlack   = PURRR_SAMPLER_BORDER_COLOR_INT_TRANSPARENT_BLACK,
+    FloatOpaqueBlack      = PURRR_SAMPLER_BORDER_COLOR_FLOAT_OPAQUE_BLACK,
+    IntOpaqueBlack        = PURRR_SAMPLER_BORDER_COLOR_INT_OPAQUE_BLACK,
+    FloatOpaqueWhite      = PURRR_SAMPLER_BORDER_COLOR_FLOAT_OPAQUE_WHITE,
+    IntOpaqueWhite        = PURRR_SAMPLER_BORDER_COLOR_INT_OPAQUE_WHITE
   };
 
   struct CreateInfo {
@@ -44,9 +44,9 @@ private:
   inline Sampler(Purrr_Context context, const CreateInfo &createInfo)
   {
     Result result = purrr_create_sampler(context, Purrr_Sampler_Create_Info{
-      (Purrr_Filter)createInfo.magFilter,
-      (Purrr_Filter)createInfo.minFilter,
-      (Purrr_Filter)createInfo.mipFilter,
+      (Purrr_Sampler_Filter)createInfo.magFilter,
+      (Purrr_Sampler_Filter)createInfo.minFilter,
+      (Purrr_Sampler_Filter)createInfo.mipFilter,
       (Purrr_Sampler_Address_Mode)createInfo.addressModeU,
       (Purrr_Sampler_Address_Mode)createInfo.addressModeV,
       (Purrr_Sampler_Address_Mode)createInfo.addressModeW,
